556435f495df6d0c3fde66f49f8a8e1579a8b035
SaadVSP96 <saadmasood192@gmail.com>
2024-06-09 02:14:24 +0500

    13. changed the main branch code to match the one on boot.dev

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -38,47 +38,60 @@ def extract_markdown_links(text: str) -> list[tuple]:
     matches = re.findall(r"\[(.*?)\]\((.*?)\)", text)
     return matches
 
-def split_nodes_image(old_nodes: list[TextNode]) -> list[TextNode]:
+
+def split_nodes_image(old_nodes):
     new_nodes = []
     for old_node in old_nodes:
-        if old_node.TEXT_TYPE != text_type_text:
+        if old_node.text_type != text_type_text:
             new_nodes.append(old_node)
             continue
-        matches = re.findall(r"(.*?)!\[(.*?)\]\((.*?)\)", old_node.TEXT)
-        for match in matches:
-            i = 0
-            while i < len(match):
-                if not match[i]:
-                    i +=1 
-                    continue
-                elif match[i] and ("image" not in match[i]):
-                    new_nodes.append(TextNode(match[i], text_type_text))
-                    i += 1
-                elif "image" in match[i]:
-                    new_nodes.append(TextNode(match[i], text_type_image, match[i+1]))
-                    i += 2
+        original_text = old_node.text
+        images = extract_markdown_images(original_text)
+        if len(images) == 0:
+            new_nodes.append(old_node)
+            continue
+        for image in images:
+            sections = original_text.split(f"![{image[0]}]({image[1]})", 1)
+            if len(sections) != 2:
+                raise ValueError("Invalid markdown, image section not closed")
+            if sections[0] != "":
+                new_nodes.append(TextNode(sections[0], text_type_text))
+            new_nodes.append(
+                TextNode(
+                    image[0],
+                    text_type_image,
+                    image[1],
+                )
+            )
+            original_text = sections[1]
+        if original_text != "":
+            new_nodes.append(TextNode(original_text, text_type_text))
     return new_nodes
 
-def split_nodes_link(old_nodes: list[TextNode]) -> list[TextNode]:
+
+def split_nodes_link(old_nodes):
     new_nodes = []
     for old_node in old_nodes:
-        if old_node.TEXT_TYPE != text_type_text:
+        if old_node.text_type != text_type_text:
             new_nodes.append(old_node)
             continue
-        matches = re.findall(r"(.*?)\[(.*?)\]\((.*?)\)", old_node.TEXT)
-        for match in matches:
-            i = 0
-            while i < len(match):
-                if not match[i]:
-                    i +=1 
-                    continue
-                elif match[i] and ("link" not in match[i]):
-                    new_nodes.append(TextNode(match[i], text_type_text))
-                    i += 1
-                elif "link" in match[i]:
-                    new_nodes.append(TextNode(match[i], text_type_link, match[i+1]))
-                    i += 2
+        original_text = old_node.text
+        links = extract_markdown_links(original_text)
+        if len(links) == 0:
+            new_nodes.append(old_node)
+            continue
+        for link in links:
+            sections = original_text.split(f"[{link[0]}]({link[1]})", 1)
+            if len(sections) != 2:
+                raise ValueError("Invalid markdown, link section not closed")
+            if sections[0] != "":
+                new_nodes.append(TextNode(sections[0], text_type_text))
+            new_nodes.append(TextNode(link[0], text_type_link, link[1]))
+            original_text = sections[1]
+        if original_text != "":
+            new_nodes.append(TextNode(original_text, text_type_text))
     return new_nodes
+
 
 node = TextNode(
     "This is text with a [link](https://boot.dev) and [another link](https://blog.boot.dev) with text that follows",
