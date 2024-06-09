61791e0d53c8934df9a12e6c79e526732b663995
SaadVSP96 <saadmasood192@gmail.com>
2024-06-09 15:19:11 +0500

    16. did some cleanup in inline_markdown and added a test to test_inline_markdown

--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -1,11 +1,12 @@
 import unittest
 
-from inline_markdown import (   # type: ignore
+from inline_markdown import (
     split_nodes_delimiter,
     extract_markdown_links,
     extract_markdown_images,
     split_nodes_image,
     split_nodes_link,
+    text_to_textnodes,
 )
 from textnode import (
     TextNode,
@@ -198,8 +199,24 @@ class TestInlineMarkdown(unittest.TestCase):
             new_nodes,
         )
 
-
-
+    def test_text_to_textnodes(self):
+        text = "This is **text** with an *italic* word and a `code block` and an ![image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) and a [link](https://boot.dev)"
+        new_nodes = text_to_textnodes(text)
+        self.assertListEqual(
+            [
+                TextNode("This is ", text_type_text),
+                TextNode("text", text_type_bold),
+                TextNode(" with an ", text_type_text),
+                TextNode("italic", text_type_italic),
+                TextNode(" word and a ", text_type_text),
+                TextNode("code block", text_type_code),
+                TextNode(" and an ", text_type_text),
+                TextNode("image", text_type_image, "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png"),
+                TextNode(" and a ", text_type_text),
+                TextNode("link", text_type_link, "https://boot.dev"),
+            ],
+            new_nodes,
+        )
 
 if __name__ == "__main__":
     unittest.main()
