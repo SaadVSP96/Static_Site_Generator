97174733ddee0c010e138e67c29715bd7685c29e
SaadVSP96 <saadmasood192@gmail.com>
2024-06-10 23:22:16 +0500

    33. added gencontent to make HTML from markdown

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ import os
 import shutil
 
 from copystatic import copy_files_recursive
-
+from gencontent import generate_page
 
 # Get the directory containing this script
 script_dir = os.path.abspath(os.path.dirname(__file__))
@@ -10,14 +10,22 @@ script_dir = os.path.abspath(os.path.dirname(__file__))
 # Define your relative paths
 relative_dir_path_static = '../static'
 relative_dir_path_public = '../public'
+relative_dir_path_content = '../content'
+relative_path_template = '../template.html'
+
 
 # Convert them to absolute paths
 dir_path_static = os.path.join(script_dir, relative_dir_path_static)
 dir_path_public = os.path.join(script_dir, relative_dir_path_public)
+dir_path_content = os.path.join(script_dir, relative_dir_path_content)
+dir_path_template = os.path.join(script_dir, relative_path_template)
+
 
 # Ensure the directories are normalized
 dir_path_static = os.path.normpath(dir_path_static)
 dir_path_public = os.path.normpath(dir_path_public)
+dir_path_content = os.path.normpath(dir_path_content)
+dir_path_template = os.path.normpath(dir_path_template)
 
 
 def main():
@@ -27,6 +35,13 @@ def main():
 
     print("Copying static files to public directory...")
     copy_files_recursive(dir_path_static, dir_path_public)
+
+    print("Generating page...")
+    generate_page(
+        os.path.join(dir_path_content, "index.md"),
+        dir_path_template,
+        os.path.join(dir_path_public, "index.html"),
+    )
 
 
 main()
